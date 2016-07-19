cb7a1ec5169230aeb507f2b7c748c5c35e4bd5c6
Benjamin Dant <benjamindant@gmail.com>
2016-07-19 18:20:35 +0200

    Grocery List

--- source/App.js
+++ source/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Hello extends React.Component {
+class Hello extends Component {
 	render() {
+		var place = "World";
 		return (
-			<h1>Hello World One</h1>
+			<h1>Hello {place}</h1>
 		);
 	}
 }
 
-React.render(<Hello />, document.getElementById('root'));
+class GroceryList extends Component {
+	render() {
+		return (
+			<ul>
+				<ListItem quantity="1" name="Bread" />
+				<ListItem quantity="6" name="Eggs" />
+				<ListItem quantity="2" name="Milk" />
+			</ul>
+		);
+	}
+}
+
+class ListItem extends Component {
+	render() {
+		return (
+			<li>
+				{this.props.quantity} x {this.props.name}
+			</li>
+		);
+	}
+}
+
+React.render(<GroceryList />, document.getElementById('root'));
