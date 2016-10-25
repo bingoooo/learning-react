3f5f6202ca31e8021985326e11f4ecbe40737a16
Benjamin Dant <benjamindant@gmail.com>
2016-10-25 17:10:19 +0200

    Modified App.js for kanban and index.html linking css

--- source/App.js
+++ source/App.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
 
-class Hello extends Component {
-	render() {
-		var place = "World";
-		return (
-			<h1>Hello {place}</h1>
-		);
-	}
-}
+let cardList = [
+	{
+		id: 1,
+		title: "Read the book",
+		description: "I should read the whole book",
+		status: "in-progress",
+		tasks: []
+	},
+	{
+		id: 2,
+		title: "Write some code",
+		description: "Code along with samples in the book",
+		status: "todo",
+		tasks: [
+			{
+				id: 1,
+				name: "ContactList Example",
+				done: true
+			},
+			{
+				id: 2,
+				name:"Kanban Example",
+				done: false
+			},
+			{
+				id: 3,
+				name: "My own experiments",
+				done: false
+			}
+		]
+	},
+];
 
-class GroceryList extends Component {
-	render() {
-		return (
-			<ul>
-				<ListItem quantity="1">Pain</ListItem>
-				<ListItem quantity="6">Oeufs</ListItem>
-				<ListItem quantity="2">Lait (litre)</ListItem>
-			</ul>
-		);
-	}
-}
-
-class ListItem extends Component {
-	render() {
-		return (
-			<li>
-				{this.props.quantity} x {this.props.children}
-			</li>
-		);
-	}
-}
-
-React.render(<GroceryList />, document.getElementById('root'));
+React.render(<KanbanBoard cards={cardList} />, document.getElementById('root'))
